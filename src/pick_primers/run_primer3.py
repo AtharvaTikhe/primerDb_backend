ce6afed0e0af1de9ca08a397dda08fd5fca90243
Atharva-Tikhe <atharvatikhe22@gmail.com>
2024-10-25 17:23:45 +0530

    fix: prepared pick_primer module

--- src/pick_primers/run_primer3.py
+++ src/pick_primers/run_primer3.py
@@ -2,6 +2,9 @@ import json
 import os.path
 import subprocess
 from subprocess import CalledProcessError
+
+from src.db_lookup.tabix_based_lookup import DbLookup
+from src.db_lookup.ucsc_scraper import UCSCScraper
 from src.utils.primer3_parser.primer3_output_parser import P3outputParser
 from src.utils.backend_logger.logger import BackendLogger
 
@@ -26,8 +29,8 @@ class GenerateP3Input:
         seq = GetSequence(chr, coord, flanks)
         self.seq = json.loads(seq.get_seq_from_api())['dna'].strip()
 
-        self.p3_input_file = f"{cache_path}/{self.seq_id}.input.txt"
-        self.p3_output = f"{output_path}/{self.seq_id}_out.txt"
+        self.p3_input_file = f"{cache_path}/{self.seq_id}.{self.num_ret}.input.txt"
+        self.p3_output = f"{output_path}/{self.seq_id}_{self.num_ret}_out.txt"
         self.logger = BackendLogger()
 
 
@@ -71,14 +74,25 @@ class GenerateP3Input:
 
                 # parse entire output and store as JSON
                 parsed_out = P3outputParser(f"{self.p3_output}")
+
                 with open(f"{output_path}/{self.seq_id}_p3_out.json", 'w') as f:
                     json.dump(json.loads(parsed_out.parse_file()), f, ensure_ascii=False, indent=4)
 
-                return json.loads(primer_seq.json)
+                return json.loads(primer_seq.json), json.loads(parsed_out.parse_file())
                 # print(json.loads(primer_seq.json))
         except CalledProcessError:
             self.logger.general_log(f'primer3 failed for {self.p3_input_file}')
 
 if __name__=="__main__":
-    obj = GenerateP3Input('chr13', '20189511', '1000', 'CHR13', '900,200', '10')
-    print(obj.run_primer3())
+    chr = 'chr13'
+    obj = GenerateP3Input(chr, '20189511', '1000', 'CHR13', '900,200', '1')
+    primer_pairs, full_output = obj.run_primer3()
+
+    print(primer_pairs)
+    for key, value in primer_pairs.items():
+        obj = UCSCScraper(f'{chr}_{int(key)}', value['left_primer'], value['right_primer'])
+        db_obj = DbLookup(obj.get_coords(), f'{int(key)}')
+        print(key)
+        print('--------------------------')
+        print(db_obj.results)
+        print(db_obj.parse_results())
