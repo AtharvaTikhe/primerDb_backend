48bb7e3fdcd1cc6c10b70b5c4a3372177a023130
Atharva-Tikhe <atharvatikhe22@gmail.com>
2024-11-15 09:03:22 +0530

    fix: config handler

--- src/utils/config_parser/config_parser.py
+++ src/utils/config_parser/config_parser.py
@@ -1,20 +1,24 @@
 import configparser
 def parse_config(category):
     config = configparser.ConfigParser()
-    config.read('/Users/atharvatikhe/MedGenome/primerDb_backend/src/config.ini')
+    config.read('/home/atharvatikhe/primerDb_backend/src/config.ini')
     config_values = {}
     try:
-        config_values['cache_path'] = config.get(category, 'cache_path')
-        config_values['output_path'] = config.get(category, 'output_path')
-        config_values['primer3_bin'] = config.get(category, 'primer3_bin')
         if category == 'Pick_primers':
+            config_values['cache_path'] = config.get(category, 'cache_path')
+            config_values['output_path'] = config.get(category, 'output_path')
+            config_values['primer3_bin'] = config.get(category, 'primer3_bin')
             config_values['primer3_settings'] = config.get(category, 'primer3_settings')
         if category == 'Db_lookup':
             config_values['tabix_bin'] = config.get(category, 'tabix_bin')
             config_values['db_root'] = config.get(category, 'db_root')
         if category == 'logger':
             config_values['log_path'] = config.get(category, 'log_path')
-
+        if category == 'Check_primers':
+            config_values['cache_path'] = config.get(category, 'cache_path')
+            config_values['output_path'] = config.get(category, 'output_path')
+            config_values['primer3_bin'] = config.get(category, 'primer3_bin')
     except configparser.NoOptionError:
-        pass
+        print(category)
+
     return config_values
