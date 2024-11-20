13d277a00a4b23d2c5f3f29548c4a4411dd56134
Atharva-Tikhe <atharvatikhe22@gmail.com>
2024-11-20 16:37:07 +0530

    fix: fixed 1-based indexing for UCSC url

--- src/pick_primers/get_sequence.py
+++ src/pick_primers/get_sequence.py
@@ -22,7 +22,8 @@ class GetSequence:
     def get_seq_from_api(self):
         # url = "https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom={self.chr};start={self.lcoord};end={self.rcoord}"
 
-        url = self.config['ucsc_url'].format(self.chr, self.lcoord, self.rcoord)
+        url = self.config['ucsc_url'].format(self.chr, self.lcoord - 1, self.rcoord)
+        print(url)
         self.logger.general_log(f"Requesting sequence using url {url}")
         resp = requests.get(url)
         self.logger.general_log(f"Response code {resp.status_code}")
