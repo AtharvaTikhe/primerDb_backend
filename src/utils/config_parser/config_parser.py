7732fbead871df75bc523a508796ac503715a256
Atharva-Tikhe <atharvatikhe22@gmail.com>
2024-11-22 10:35:45 +0530

    Feat: Added gnomad root to config

--- src/utils/config_parser/config_parser.py
+++ src/utils/config_parser/config_parser.py
@@ -3,6 +3,7 @@ def parse_config(category):
     config = configparser.ConfigParser()
     config.read('./src/config.ini')
     config_values = {}
+
     try:
         if category == 'Pick_primers':
             config_values['cache_path'] = config.get(category, 'cache_path')
@@ -13,6 +14,7 @@ def parse_config(category):
         if category == 'Db_lookup':
             config_values['tabix_bin'] = config.get(category, 'tabix_bin')
             config_values['db_root'] = config.get(category, 'db_root')
+            config_values['gnomad_root'] = config.get(category, 'gnomad_root')
         if category == 'Log_path':
             config_values['log_path'] = config.get(category, 'log_path')
         if category == 'Check_primers':
