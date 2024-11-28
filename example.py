66bc62d71bfd8d51e25856cd8ef3dbcd0818d0f9
Atharva-Tikhe <atharvatikhe22@gmail.com>
2024-11-28 12:01:41 +0530

    chore: updated example file

--- example.py
+++ example.py
@@ -6,10 +6,10 @@ For Check primer functionality - pass sequence ID and sequences.
 The returned string has to be loaded as JSON. The JSON contains DB data as well.
 
 """
-from src.check_primers.check_primer import CheckPrimer
-obj = CheckPrimer("TEST1", "CACACGTTCTTGCAGCCTG", "TCCTGTGTTGTGTGCATTCG")
-out = json.loads(obj.run_primer3())
-print(out)
+# from src.check_primers.check_primer import CheckPrimer
+# obj = CheckPrimer("TEST1", "CACACGTTCTTGCAGCCTG", "TCCTGTGTTGTGTGCATTCG")
+# out = json.loads(obj.run_primer3())
+# print(out)
 
 """
 
@@ -19,6 +19,15 @@ primer_pairs JSON now has database results as well.
 
 """
 from src.pick_primers.run_primer3 import GenerateP3Input
-obj = GenerateP3Input('chr1', "15529554", '1000', 'CHR1', '900,200', '5')
+
+obj = GenerateP3Input('chr1', "15529554", '1000', 'CHR1', '900,200', '1')
+
 primer_pairs, full_output = obj.run_primer3()
 print(primer_pairs)
+
+# from src.utils.Db.dbInteract import CheckEntry
+# db_entry = CheckEntry('chr1', "15529554", '1000', 'CHR1', '900,200', '2', primer_pairs)
+# print(db_entry.check_entry(add_if_absent=True))
+# print(db_entry.query_all())
+
+
